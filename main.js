d64bc0282795f56326950248b5f8472fe449b8ec
as9826372655 <89297733+as9826372655@users.noreply.github.com>
2022-02-17 08:10:53 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+var canvas = new fabric.Canvas('myCanvas');
+
+ var x = document.getElementById("myAudio");
+
+function new_image()
+{
+
+fabric.Image.fromURL("Bd.jpg", function(Img){
+    block_object = Img;
+    block_object.scaleToWidth(700);
+    block_object.scaleToHeight(510);
+    block_object.set({
+        top:0,
+        left:0
+    });
+    canvas.add(block_object)
+});
+	
+}
+
+function playsound(){
+	x.play();
+}
